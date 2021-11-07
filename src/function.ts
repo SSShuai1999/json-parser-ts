98886873d97d7f1993e5fe8d1f94e1ceb77122c7
SSShuai1999 <ssshuai1999@gmail.com>
2021-11-07 19:20:05 +0800

    [ Feat ] type completion

--- src/function.ts
+++ src/function.ts
@@ -1,209 +1,171 @@
 import { apply, inject } from "./main";
-import { Parser, PickParserT } from "./typings";
-import { PickFuncParameterByIdx, PickFuncRet } from "./typings/utils";
+import { Parser
+       , PickParserT
+       , PickFuncParameterByIdx
+       , PickFuncRet } from "./typings";
 
-export function applyAllI
-<A extends Parser<any>,
-    B extends A,
-    C extends A>
-(fn: B,
-    a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>): 
-Parser<PickFuncRet<PickParserT<typeof fn>>>
+export function applyAllI<A extends Parser<any>, B extends A>
+  (fn: B,
+    a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>):
+    Parser<PickFuncRet<PickParserT<typeof fn>>>
 
-export function applyAllI
-<A extends Parser<any>,
-    B extends A>
-(fn: B,
-    a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
-    b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>):
-Parser<PickFuncRet<PickParserT<typeof fn>>>
+export function applyAllI<A extends Parser<any>, B extends A>
+   (fn: B,
+     a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
+     b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>):
+    Parser<PickFuncRet<PickParserT<typeof fn>>>
 
-export function applyAllI
-<A extends Parser<any>,
-    B extends A>
-(fn: B,
-    a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
-    b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
-    c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>):
-Parser<PickFuncRet<PickParserT<typeof fn>>>
+export function applyAllI<A extends Parser<any>, B extends A>
+   (fn: B,
+     a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
+     b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
+     c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>):
+    Parser<PickFuncRet<PickParserT<typeof fn>>>
 
-export function applyAllI
-<A extends Parser<any>,
-    B extends A>
-(fn: B,
-    a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
-    b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
-    c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
-    d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>):
-Parser<PickFuncRet<PickParserT<typeof fn>>>
+export function applyAllI<A extends Parser<any>, B extends A>
+   (fn: B,
+     a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
+     b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
+     c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
+     d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>):
+    Parser<PickFuncRet<PickParserT<typeof fn>>>
 
-export function applyAllI
-<A extends Parser<any>,
-    B extends A>
-(fn: B,
-    a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
-    b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
-    c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
-    d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
-    e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>):
-Parser<PickFuncRet<PickParserT<typeof fn>>>
+export function applyAllI<A extends Parser<any>, B extends A>
+   (fn: B,
+     a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
+     b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
+     c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
+     d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
+     e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>):
+    Parser<PickFuncRet<PickParserT<typeof fn>>>
 
-export function applyAllI
-<A extends Parser<any>,
-    B extends A>
-(fn: B,
-    a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
-    b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
-    c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
-    d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
-    e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
-    f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>):
-Parser<PickFuncRet<PickParserT<typeof fn>>>
+export function applyAllI<A extends Parser<any>, B extends A>
+   (fn: B,
+     a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
+     b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
+     c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
+     d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
+     e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
+     f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>):
+    Parser<PickFuncRet<PickParserT<typeof fn>>>
 
-export function applyAllI
-<A extends Parser<any>,
-    B extends A>
-(fn: B,
-    a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
-    b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
-    c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
-    d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
-    e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
-    f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>,
-    g: Parser<PickFuncParameterByIdx<PickParserT<B>, 7>>):
-Parser<PickFuncRet<PickParserT<typeof fn>>>
+export function applyAllI<A extends Parser<any>, B extends A>
+   (fn: B,
+     a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
+     b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
+     c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
+     d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
+     e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
+     f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>,
+     g: Parser<PickFuncParameterByIdx<PickParserT<B>, 7>>):
+    Parser<PickFuncRet<PickParserT<typeof fn>>>
 
-export function applyAllI
-<A extends Parser<any>,
-    B extends A>
-(fn: B,
-    a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
-    b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
-    c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
-    d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
-    e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
-    f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>,
-    g: Parser<PickFuncParameterByIdx<PickParserT<B>, 7>>,
-    h: Parser<PickFuncParameterByIdx<PickParserT<B>, 8>>):
-Parser<PickFuncRet<PickParserT<typeof fn>>>
+export function applyAllI<A extends Parser<any>, B extends A>
+   (fn: B,
+     a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
+     b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
+     c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
+     d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
+     e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
+     f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>,
+     g: Parser<PickFuncParameterByIdx<PickParserT<B>, 7>>,
+     h: Parser<PickFuncParameterByIdx<PickParserT<B>, 8>>):
+    Parser<PickFuncRet<PickParserT<typeof fn>>>
 
-export function applyAllI
-<A extends Parser<any>,
-    B extends A>
-(fn: B,
-    a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
-    b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
-    c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
-    d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
-    e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
-    f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>,
-    g: Parser<PickFuncParameterByIdx<PickParserT<B>, 7>>,
-    h: Parser<PickFuncParameterByIdx<PickParserT<B>, 8>>,
-    i: Parser<PickFuncParameterByIdx<PickParserT<B>, 9>>):
-Parser<PickFuncRet<PickParserT<typeof fn>>>
+export function applyAllI<A extends Parser<any>, B extends A>
+   (fn: B,
+     a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
+     b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
+     c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
+     d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
+     e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
+     f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>,
+     g: Parser<PickFuncParameterByIdx<PickParserT<B>, 7>>,
+     h: Parser<PickFuncParameterByIdx<PickParserT<B>, 8>>,
+     i: Parser<PickFuncParameterByIdx<PickParserT<B>, 9>>):
+    Parser<PickFuncRet<PickParserT<typeof fn>>>
 
-export function applyAllI
-<A extends Parser<any>,
-    B extends A>
-(fn: B,
-    a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
-    b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
-    c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
-    d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
-    e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
-    f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>,
-    g: Parser<PickFuncParameterByIdx<PickParserT<B>, 7>>,
-    h: Parser<PickFuncParameterByIdx<PickParserT<B>, 8>>,
-    i: Parser<PickFuncParameterByIdx<PickParserT<B>, 9>>,
-    j: Parser<PickFuncParameterByIdx<PickParserT<B>, 10>>):
-Parser<PickFuncRet<PickParserT<typeof fn>>>
+export function applyAllI<A extends Parser<any>, B extends A>
+   (fn: B,
+     a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
+     b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
+     c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
+     d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
+     e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
+     f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>,
+     g: Parser<PickFuncParameterByIdx<PickParserT<B>, 7>>,
+     h: Parser<PickFuncParameterByIdx<PickParserT<B>, 8>>,
+     i: Parser<PickFuncParameterByIdx<PickParserT<B>, 9>>,
+     j: Parser<PickFuncParameterByIdx<PickParserT<B>, 10>>):
+    Parser<PickFuncRet<PickParserT<typeof fn>>>
 
-export function applyAllI
-<A extends Parser<any>,
-    B extends A>
-(fn: B,
-    a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
-    b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
-    c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
-    d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
-    e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
-    f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>,
-    g: Parser<PickFuncParameterByIdx<PickParserT<B>, 7>>,
-    h: Parser<PickFuncParameterByIdx<PickParserT<B>, 8>>,
-    i: Parser<PickFuncParameterByIdx<PickParserT<B>, 9>>,
-    j: Parser<PickFuncParameterByIdx<PickParserT<B>, 10>>,
-    k: Parser<PickFuncParameterByIdx<PickParserT<B>, 11>>):
-Parser<PickFuncRet<PickParserT<typeof fn>>>
+export function applyAllI<A extends Parser<any>, B extends A>
+   (fn: B,
+     a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
+     b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
+     c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
+     d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
+     e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
+     f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>,
+     g: Parser<PickFuncParameterByIdx<PickParserT<B>, 7>>,
+     h: Parser<PickFuncParameterByIdx<PickParserT<B>, 8>>,
+     i: Parser<PickFuncParameterByIdx<PickParserT<B>, 9>>,
+     j: Parser<PickFuncParameterByIdx<PickParserT<B>, 10>>,
+     k: Parser<PickFuncParameterByIdx<PickParserT<B>, 11>>):
+    Parser<PickFuncRet<PickParserT<typeof fn>>>
 
-export function applyAllI
-<A extends Parser<any>,
-    B extends A>
-(fn: B,
-    a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
-    b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
-    c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
-    d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
-    e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
-    f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>,
-    g: Parser<PickFuncParameterByIdx<PickParserT<B>, 7>>,
-    h: Parser<PickFuncParameterByIdx<PickParserT<B>, 8>>,
-    i: Parser<PickFuncParameterByIdx<PickParserT<B>, 9>>,
-    j: Parser<PickFuncParameterByIdx<PickParserT<B>, 10>>,
-    k: Parser<PickFuncParameterByIdx<PickParserT<B>, 11>>,
-    l: Parser<PickFuncParameterByIdx<PickParserT<B>, 12>>):
-Parser<PickFuncRet<PickParserT<typeof fn>>>
+export function applyAllI<A extends Parser<any>, B extends A>
+   (fn: B,
+     a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
+     b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
+     c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
+     d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
+     e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
+     f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>,
+     g: Parser<PickFuncParameterByIdx<PickParserT<B>, 7>>,
+     h: Parser<PickFuncParameterByIdx<PickParserT<B>, 8>>,
+     i: Parser<PickFuncParameterByIdx<PickParserT<B>, 9>>,
+     j: Parser<PickFuncParameterByIdx<PickParserT<B>, 10>>,
+     k: Parser<PickFuncParameterByIdx<PickParserT<B>, 11>>,
+     l: Parser<PickFuncParameterByIdx<PickParserT<B>, 12>>):
+    Parser<PickFuncRet<PickParserT<typeof fn>>>
 
-export function applyAllI
-<A extends Parser<any>,
-    B extends A>
-(fn: B,
-    a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
-    b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
-    c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
-    d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
-    e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
-    f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>,
-    g: Parser<PickFuncParameterByIdx<PickParserT<B>, 7>>,
-    h: Parser<PickFuncParameterByIdx<PickParserT<B>, 8>>,
-    i: Parser<PickFuncParameterByIdx<PickParserT<B>, 9>>,
-    j: Parser<PickFuncParameterByIdx<PickParserT<B>, 10>>,
-    k: Parser<PickFuncParameterByIdx<PickParserT<B>, 11>>,
-    l: Parser<PickFuncParameterByIdx<PickParserT<B>, 12>>,
-    m: Parser<PickFuncParameterByIdx<PickParserT<B>, 13>>):
-Parser<PickFuncRet<PickParserT<typeof fn>>>
+export function applyAllI<A extends Parser<any>, B extends A>
+   (fn: B,
+     a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
+     b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
+     c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
+     d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
+     e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
+     f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>,
+     g: Parser<PickFuncParameterByIdx<PickParserT<B>, 7>>,
+     h: Parser<PickFuncParameterByIdx<PickParserT<B>, 8>>,
+     i: Parser<PickFuncParameterByIdx<PickParserT<B>, 9>>,
+     j: Parser<PickFuncParameterByIdx<PickParserT<B>, 10>>,
+     k: Parser<PickFuncParameterByIdx<PickParserT<B>, 11>>,
+     l: Parser<PickFuncParameterByIdx<PickParserT<B>, 12>>,
+     m: Parser<PickFuncParameterByIdx<PickParserT<B>, 13>>):
+    Parser<PickFuncRet<PickParserT<typeof fn>>>
 
-export function applyAllI
-<A extends Parser<any>,
-    B extends A>
-(fn: B,
-    a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
-    b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
-    c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
-    d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
-    e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
-    f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>,
-    g: Parser<PickFuncParameterByIdx<PickParserT<B>, 7>>,
-    h: Parser<PickFuncParameterByIdx<PickParserT<B>, 8>>,
-    i: Parser<PickFuncParameterByIdx<PickParserT<B>, 9>>,
-    j: Parser<PickFuncParameterByIdx<PickParserT<B>, 10>>,
-    k: Parser<PickFuncParameterByIdx<PickParserT<B>, 11>>,
-    l: Parser<PickFuncParameterByIdx<PickParserT<B>, 12>>,
-    m: Parser<PickFuncParameterByIdx<PickParserT<B>, 13>>,
-    n: Parser<PickFuncParameterByIdx<PickParserT<B>, 14>>):
-Parser<PickFuncRet<PickParserT<typeof fn>>>
+export function applyAllI<A extends Parser<any>, B extends A>
+   (fn: B,
+     a: Parser<PickFuncParameterByIdx<PickParserT<B>, 1>>,
+     b: Parser<PickFuncParameterByIdx<PickParserT<B>, 2>>,
+     c: Parser<PickFuncParameterByIdx<PickParserT<B>, 3>>,
+     d: Parser<PickFuncParameterByIdx<PickParserT<B>, 4>>,
+     e: Parser<PickFuncParameterByIdx<PickParserT<B>, 5>>,
+     f: Parser<PickFuncParameterByIdx<PickParserT<B>, 6>>,
+     g: Parser<PickFuncParameterByIdx<PickParserT<B>, 7>>,
+     h: Parser<PickFuncParameterByIdx<PickParserT<B>, 8>>,
+     i: Parser<PickFuncParameterByIdx<PickParserT<B>, 9>>,
+     j: Parser<PickFuncParameterByIdx<PickParserT<B>, 10>>,
+     k: Parser<PickFuncParameterByIdx<PickParserT<B>, 11>>,
+     l: Parser<PickFuncParameterByIdx<PickParserT<B>, 12>>,
+     m: Parser<PickFuncParameterByIdx<PickParserT<B>, 13>>,
+     n: Parser<PickFuncParameterByIdx<PickParserT<B>, 14>>):
+    Parser<PickFuncRet<PickParserT<typeof fn>>>
 
 export function applyAllI(...args) {
     return args.reduce(apply)
 }
 
-/** e.g. */
-
-type Person = {
-    name: string,
-    age: number
-}
-
-const p1: Person = { name: "松鼠", age: 22 };
-const res = applyAllI(inject(((p: Person) => p)), inject(p1))
-
-console.log(res) // -> : Parser<Person>
--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,13 +1,15 @@
 
-export type MaybeError = "empty source" 
+export type AtLeastOnceError = `At least match once`
+
+export type MaybeError = "empty source" | `Unexpect char ${any}` | AtLeastOnceError
 
 export type AST<A, B> = [A, B]
 
 export type Maybe<A, B> = AST<A, B> | MaybeError
 
-export type ParserFunc<A> = <B>(source: B) => Maybe<A, B>
+export type ParserFunc<A> = (source: string) => Maybe<A, string>
 
-export type Parser<A> = <B>(source: B) => Maybe<A, B>
+export type Parser<A> = (source: string) => Maybe<A, string>
 
 /** 解构出 Parser */
 export type PickParser<A> = A extends Parser<infer B> ? Parser<B> : never
@@ -15,7 +17,53 @@ export type PickParser<A> = A extends Parser<infer B> ? Parser<B> : never
 /** 解构出 Parser 的泛型 */
 export type PickParserT<A> = A extends Parser<infer B> ? B : never
 
+/** 传入具体的参数类型，构造一个函数 */
+export type generatorFunc<A, B> = (arg: A) => B
 
+/** 取出函数的多个参数类型 */
+export type PickFuncParameters<A extends Function> = A extends (...args: infer B) => any ? B : never 
+
+/** 取出函数第一个参数类型 */
+export type PickFuncParameter<A extends Function> = A extends (arg: infer B) => any ? B : never 
+
+/** 取出函数对应的第 N 个参数类型
+ * 例子：const fn = (a: number) => (b: string) => (c: boolean) => boolean
+ * PickFuncParameterByIdx<A, 1> -> number 
+ * PickFuncParameterByIdx<A, 2> -> string 
+ * PickFuncParameterByIdx<A, 1> -> boolean 
+ */
+export type PickFuncParameterByIdx
+            <A extends Function,
+             M extends Number = 1,
+             I extends Number[] = [0]> = 
+    A extends (arg: infer C) => infer B ? 
+                                I['length'] extends M ? 
+                                    C : B extends Function ? 
+                                       PickFuncParameterByIdx<B, M, [...I, 0]> : never
+                                : never 
+
+/** 取出函数的返回值类型 */
+export type PickFuncRet<A extends Function> = 
+    A extends (...args: any) => infer B ? B extends Function ? PickFuncRet<B> : B : never 
+
+export type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
+
+export type XOR<T, U> = T | U extends object ? 
+                              (Without<T, U> & U) | (Without<U, T> & T)
+                              : T | U;
+
+// 返回类型为具体值的函数
+export type FuncType<A extends boolean | string | number, B extends unknown = unknown[]> = {
+    "boolean": B extends unknown[] ? (...args: B) => boolean : (args: B) => boolean,
+    "string" : B extends unknown[] ? (...args: B) => string : (...args: B[]) => string,
+    "number" : B extends unknown[] ? (...args: B) => number : (...args: B[]) => number,
+}[A extends boolean ? 
+        "boolean" : 
+            A extends string ? 
+                "string" : 
+                    A extends number ? 
+                        "number" 
+                            : never]
 
 
 
